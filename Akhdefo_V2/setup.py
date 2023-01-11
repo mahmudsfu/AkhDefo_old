dd7aa81cf062feae2d60212bc90b13dfe62fe4c5
Mahmud <102554788+mahmudsfu@users.noreply.github.com>
2023-01-11 13:03:26 -0800

    docs

--- Akhdefo_V2/setup.py
+++ Akhdefo_V2/setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup
+from sphinx.setup_command import BuildDoc
+cmdclass = {'build_sphinx': BuildDoc}
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='akhdefo_functions',
-    version='2.1.01',    
+    version='2.2.2',    
     description='Land Deformation Monitoring Using Optical Satellite Imagery',
     url='https://github.com/mahmudsfu/AkhDefo',
     author='Mahmud Mustafa Muhammad',
     author_email='mahmud.muhamm1@gmail.com',
     license='Academic Free License (AFL)',
-    package_dir={"": "src"},
+    packages=['akhdefo_functions'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    py_modules=["Akhdefo_Coreg", 'AkhDefo_Tools', 'AkhDefo_TS', 'Akhdefo_utils'
-    , 'AkhdefoPlot','Filter_PreProc','Mosaic_Crop','OpticalFlow', 'Stacked_Velocity', 'Unzip_CopyFiles' ],
     zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
